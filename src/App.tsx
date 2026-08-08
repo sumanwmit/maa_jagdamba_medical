09fc7472ebf32561310ac1c0265e6289509664d3
sumanwmit <suman.wmit@gmail.com>
2026-08-08 16:29:59 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,7 @@ export default function App() {
 
   // Global Tracking Hook to track React SPA page changes automatically
   useEffect(() => {
-    const TRACKING_ENDPOINT = 'https://tools.cprajapati.com/tracker/track.php';
+    const TRACKING_ENDPOINT = 'https://crm.webmakerit.com/tracker/track.php';
     const urlParams = new URLSearchParams(window.location.search);
     let cid = urlParams.get('cid') || localStorage.getItem('wmit_active_cid');
     if (urlParams.get('cid')) {
